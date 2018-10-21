c6a0cf5ce406f50bbf6f51cad3aba6edc19cb384
Dielan <dielano@hotmail.be>
2018-10-21 13:40:59 +0200

    Mark1

--- js/parallax.js
+++ js/parallax.js
@@ -7,5 +7,9 @@ function parallaxScroll(){
 	$('.city').css('top',(0+(scrolled*.25))+'px');
   $('.air').css('top',(0+(scrolled*.35))+'px');
   $('.sun').css('top',(0+(scrolled*.5))+'px');
-	$('.trees').css('top',(0+(scrolled*.15))+'px');
+  $('.trees').css('top',(0+(scrolled*.15))+'px');
+  $('header').css('top',(0+(scrolled*.55))+'px');
+  $('.mountain_air').css('top',(-150+(scrolled*.1))+'px');
+  $('.mountains').css('top',(-250+(scrolled*.15))+'px');
+	$('.walkers').css('top',(-400+(scrolled*.25))+'px');
 }
