e72ea4458a0455b109dd4e72c0265c60e8e74d22
YannisWS <yannis.wellemans@hotmail.com>
2018-10-23 11:56:29 +0200

    Merch V1
    - Added Merchandise
    - Changed AutoScroll Timing

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
             if (target.length) {
                 $('html, body').animate({
                     scrollTop: target.offset().top
-                }, 1000);
+                }, 1500);
                 return false;
             }
         }
