0e2434467038026820f1699e336769f35dd67861
YannisWS <yannis.wellemans@hotmail.com>
2018-10-23 11:25:31 +0200

    Nav V3 + Parallax V2
    - Changed AutoScroll (+script.js)
    - Changed Scene3 Heights (better fit in screen + testing required)
    - Changed Balloon heights (better fit in screen + testing required)
    - Added Keyframe webkits

--- js/parallax.js
+++ js/parallax.js
@@ -1,16 +1,16 @@
 $(window).bind('scroll',function(e){
-  parallaxScroll();
- });
+    parallaxScroll();
+});
 
 function parallaxScroll(){
 	var scrolled = $(window).scrollTop();
 	$('.city').css('top',(0+(scrolled*.25))+'px');
-  $('.bird1, .bird3, .bird5, .balloon2').css('top',(0+(scrolled*.35))+'px');
-  $('.bird2, .bird4, .balloon1').css('top',(0+(scrolled*.5))+'px');
-  $('.sun').css('top',(0+(scrolled*.5))+'px');
-  $('.trees').css('top',(0+(scrolled*.15))+'px');
-  $('header').css('top',(0+(scrolled*.55))+'px');
-  $('.mountain_air').css('top',(-150+(scrolled*.1))+'px');
-  $('.mountains').css('top',(-250+(scrolled*.15))+'px');
+    $('.bird1, .bird3, .bird5, .balloon2').css('top',(0+(scrolled*.35))+'px');
+    $('.bird2, .bird4, .balloon1').css('top',(0+(scrolled*.5))+'px');
+    $('.sun').css('top',(0+(scrolled*.5))+'px');
+    $('.trees').css('top',(0+(scrolled*.15))+'px');
+    $('header').css('top',(0+(scrolled*.55))+'px');
+    $('.mountain_air').css('top',(-150+(scrolled*.1))+'px');
+    $('.mountains').css('top',(-250+(scrolled*.15))+'px');
 	$('.walkers').css('top',(-400+(scrolled*.25))+'px');
 }
