371f6b1d3569128ff876ec58c891f3ca3214005c
dielanophals <43468300+dielanophals@users.noreply.github.com>
2018-11-03 18:48:04 +0100

    Desktop complete

--- js/parallax.js
+++ js/parallax.js
@@ -10,9 +10,7 @@ function parallaxScroll(){
   $('.sun').css('top',(0+(scrolled*.5))+'px');
   $('.trees').css('top',(0+(scrolled*.15))+'px');
   $('header').css('top',(0+(scrolled*.55))+'px');
-  $('.clouds').css('top',(-200+(scrolled*.1))+'px');
-  $('.mountains').css('top',(-250+(scrolled*.15))+'px');
-	$('.walkers').css('top',(-400+(scrolled*.1))+'px');
+	$('.walkers').css('top',(0+(scrolled*.2))+'px');
 }
 
 $(window).scroll(function() {
